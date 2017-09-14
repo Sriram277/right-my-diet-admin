72a0b748f8380a4c16a8feef37b9306d5e92a381
Vasudev Paila <Vasudev Paila>
2017-09-14 22:25:47 +0530

    Worked on login page and styles

--- src/app/categoryList/categoryList-routing.module.ts
+++ src/app/categoryList/categoryList-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: categoryListComponent,
     data: {
-      title: 'Tips'
+      title: 'Articles'
     }
   }
 ];
--- src/app/tipsLists/tipsLists-routing.module.ts
+++ src/app/tipsLists/tipsLists-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: tipsListComponent,
     data: {
-      title: 'Tips'
+      title: 'Articles'
     }
   }
 ];
